53fa4fb3abd598ba89bf6172f4961014968aa41a
Scheffi <kevin@cortellini.de>
2021-06-02 14:45:30 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"testprojekt adapter settings": {
+		"en": "Adapter settings for testprojekt",
+		"de": "Adaptereinstellungen für testprojekt",
+		"ru": "Настройки адаптера для testprojekt",
+		"pt": "Configurações do adaptador para testprojekt",
+		"nl": "Adapterinstellingen voor testprojekt",
+		"fr": "Paramètres d'adaptateur pour testprojekt",
+		"it": "Impostazioni dell'adattatore per testprojekt",
+		"es": "Ajustes del adaptador para testprojekt",
+		"pl": "Ustawienia adaptera dla testprojekt",
+		"zh-cn": "testprojekt的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
